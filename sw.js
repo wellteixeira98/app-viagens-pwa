5332dbb661e95cb8aa5dfd4289bed73cac625fe8
wellteixeira98 <wteixeira2018@gmail.com>
2026-03-26 14:26:20 -0300

    🚀 Auto-Deploy: Atualizando sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'viagens-v20260326_1425';
+const CACHE_NAME = 'viagens-v20260326_1426';
 const urlsToCache = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', event => {
